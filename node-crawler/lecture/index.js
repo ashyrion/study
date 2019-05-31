a0c707c6f29c7bc2d31f8ffbe4345ca414a348cf
ashyrion <ashyrion@naver.com>
2019-06-01 00:21:57 +0900

    study crawler Day1

--- node-crawler/lecture/index.js
+++ node-crawler/lecture/index.js
@@ -3,6 +3,7 @@
 const xlsx = require("xlsx");
 const axios = require("axios"); // ajax
 const cheerio = require("cheerio"); // html 파싱
+const add_to_sheet = require("./add_to_sheet");
 
 // const csv = fs.readFileSync("csv/data.csv");
 // const records = parse(csv.toString("utf-8"));
@@ -11,10 +12,10 @@ const cheerio = require("cheerio"); // html 파싱
 // });
 
 const workBook = xlsx.readFile("xlsx/data.xlsx");
-console.log(workBook.SheetNames);
-for (const name of workBook.SheetNames) {
-  const ws = workBook.Sheets[name];
-} // 시트별로 진행
+// console.log(workBook.SheetNames);
+// for (const name of workBook.SheetNames) {
+//   const ws = workBook.Sheets[name];
+// } // 시트별로 진행
 
 //a1:b11 -> a2:b11
 // ws["!ref"] = ws["!ref"]
@@ -27,8 +28,9 @@ for (const name of workBook.SheetNames) {
 //   })
 //   .join(":");
 // console.log(ws["!ref"]);
-// const records = xlsx.utils.sheet_to_json(ws, { header: "A" });
-// console.log(records);
+const ws = workBook.Sheets.data;
+const records = xlsx.utils.sheet_to_json(ws);
+console.log(records);
 
 // records.forEach((r, i) => {
 //   console.log(r.제목, r.링크);
@@ -38,17 +40,27 @@ for (const name of workBook.SheetNames) {
 //   console.log(i, r.제목, r.링크);
 // }
 
-// const crawler = async () => {
-//   for (const [i, r] of records.entries()) {
-//     const response = await axios.get(r.링크);
-//     if (response.status === 200) {
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-//       const text = $(".score.score_left .star_score").text();
-//       console.log(r.제목, "평점", text.trim());
-//     }
-//   }
-//   //   await Promise.all(records.map(async r => {}));
-// };
+const crawler = async () => {
+  add_to_sheet(ws, "C1", "s", "평점");
+  add_to_sheet(ws, "D1", "s", "감독");
+  for (const [i, r] of records.entries()) {
+    const response = await axios.get(r.링크);
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const text = $(".score.score_left .star_score").text();
+      const directer = $(".info_spec dd")
+        .eq(1)
+        .text();
+      console.log(r.제목, "평점", text.trim(), "감독", directer);
+      const newCellC = "C" + (i + 2);
+      const newCellD = "D" + (i + 2);
+      add_to_sheet(ws, newCellC, "n", parseFloat(text.trim()));
+      add_to_sheet(ws, newCellD, "s", directer.trim());
+    }
+  }
+  xlsx.writeFile(workBook, "xlsx/result.xlsx");
+  //   await Promise.all(records.map(async r => {}));
+};
 
-// crawler();
+crawler();
