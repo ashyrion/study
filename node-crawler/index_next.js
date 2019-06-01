a6370ec784e7711cf716fe198d7f9843a252a5f1
ashyrion <ashyrion@naver.com>
2019-06-02 01:59:44 +0900

    puppeteer

---
+++ node-crawler/index_next.js
@@ -0,0 +1,17 @@
+const parse = require("csv-parse/lib/sync");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+const csv = fs.readFileSync("csv/data.csv");
+const records = parse(csv.toString("utf-8"));
+
+const crawler = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("http://zerocho.com");
+  await page.waitFor(3000);
+  await page.close();
+  await browser.close();
+};
+
+crawler();
